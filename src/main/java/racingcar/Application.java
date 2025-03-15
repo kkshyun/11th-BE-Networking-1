dc12ce9d127e54ffdb446df1f99a092c15fb4574
kkshyun <kkshyun56@gmail.com>
2025-03-16 00:11:51 +0900

    feat(game): implement car movement and result display
    - Added move() function to randomly move cars based on a number generator
    - Implemented print() function to display each car's progress with dashes
    - Updated main logic to execute moves and display results for each round

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,7 +3,11 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
+    static Map<String,Integer> map;
     public static String[] getCars() throws IllegalArgumentException {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] cars = Console.readLine().split(",");
@@ -22,25 +26,54 @@ public class Application {
         return count;
     }
 
+    public static void move(){
+        for (Map.Entry<String,Integer> element : map.entrySet()) {
+            int num = Randoms.pickNumberInRange(0, 9);
+            if(num>=4)
+                map.replace(element.getKey(),element.getValue()+1);
+        }
+    }
+
+    public static void print(){
+        for (Map.Entry<String,Integer> element : map.entrySet()) {
+            System.out.print(element.getKey()+" : ");
+            for (int i = 0; i < element.getValue(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 자동차 이름 입력 받기
+        String[] cars;
         try{
-            getCars();
+            cars = getCars();
         } catch(IllegalArgumentException e) {
             System.out.println("잘못된 값을 입력하였습니다. 이름은 5자 이하만 가능합니다.");
             return;
         }
 
         // 자동차 이동 횟수 입력 받기
+        int moveCount = 0;
         try{
-            getMoveCount();
+            moveCount = getMoveCount();
         } catch(IllegalArgumentException e) {
             System.out.println("잘못된 값을 입력하였습니다.");
         }
 
+        map = new HashMap<>();
+        for (int i = 0; i < cars.length; i++) {
+            map.put(cars[i],0);
+        }
 
+        System.out.println("\n실행결과");
 
-
+        for (int i = 0; i < moveCount; i++) {
+            move();
+            print();
+            System.out.println();
+        }
     }
 }
