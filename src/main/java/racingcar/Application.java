fe479e366e296a5c219a9d839761b0eabf8d54ff
kkshyun <kkshyun56@gmail.com>
2025-03-15 23:22:56 +0900

    feat(input): add move count input validation
    Implemented move count input functionality with validation.
    If the user enters a negative value, an IllegalArgumentException is thrown and an error message is displayed.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,14 +14,32 @@ public class Application {
         return cars;
     }
 
+    public static int getMoveCount() throws IllegalArgumentException {
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = Integer.parseInt(Console.readLine());
+        if(count<0)
+            throw new IllegalArgumentException();
+        return count;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 자동차 이름 입력 받기
         try{
             getCars();
         } catch(IllegalArgumentException e) {
             System.out.println("잘못된 값을 입력하였습니다. 이름은 5자 이하만 가능합니다.");
             return;
         }
+
+        // 자동차 이동 횟수 입력 받기
+        try{
+            getMoveCount();
+        } catch(IllegalArgumentException e) {
+            System.out.println("잘못된 값을 입력하였습니다.");
+        }
+
+
 
 
     }
