f22053f1ca6c4cf7c87649b5bf62aff2b0626006
kkshyun <kkshyun56@gmail.com>
2025-03-15 20:08:01 +0900

    feat(input): add car name input function with validation
    - Implemented `getCars()` to receive car names as input.
    - Car names are split by commas and validated.
    - Throws `IllegalArgumentException` if a car name exceeds 5 characters.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    public static String[] getCars() throws IllegalArgumentException {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] cars = Console.readLine().split(",");
+        for (int i = 0; i < cars.length; i++) {
+            if(cars[i].length()>5)
+                throw new IllegalArgumentException();
+        }
+        return cars;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try{
+            getCars();
+        } catch(IllegalArgumentException e) {
+            System.out.println("잘못된 값을 입력하였습니다. 이름은 5자 이하만 가능합니다.");
+        }
+
+
     }
 }
